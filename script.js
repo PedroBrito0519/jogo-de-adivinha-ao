a97f5105e23a6077bedb8c77e74c173d1333adeb
Pedro Brito <156836900+PedroBrito0519@users.noreply.github.com>
2024-09-11 12:44:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function gerar(){
+    const numale = 
+}
+//exercicio 4 faça uma funçao que gere um numero aleatorio entre 1 e 100
+function numale() {
+var numero = Math.floor(Math.random() * 100) + 1;
+document.getElementById('numberale').textContent = ` ${numero}`;
+        }
